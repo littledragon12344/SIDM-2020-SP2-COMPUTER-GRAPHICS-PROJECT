525b20e995cf6102a49ea63f4ae2d05ec6a11d57
Ethan148 <54051459+Ethan148@users.noreply.github.com>
2020-02-26 17:59:17 +0800

    fixed bug for wall.cpp

--- DM2122_Week16_Files - Copy/DM2122 Prac/Source/Wall.cpp
+++ DM2122_Week16_Files - Copy/DM2122 Prac/Source/Wall.cpp
@@ -47,7 +47,7 @@ bool Wall::generateWalls(const char* file_path)
 			temp_normals.push_back(normal);
 		}
 		// if it has "g Wall" or "g wall" in the buffer, it means that the vertices and normals in temp_vertices and temp_normals are for walls.
-		else if (strncmp("g Wall", buf, 6) == 0 || strncmp("g wall", buf, 6) == 0)
+		else if (std::string(buf).find("Wall") != std::string::npos)
 		{
 			// Get the middle position of the wall
 			Vector3 position;
