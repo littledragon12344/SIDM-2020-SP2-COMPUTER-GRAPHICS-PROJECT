ad392a551c13fef5eec7c3d125e90a199f293783
Ethan148 <54051459+Ethan148@users.noreply.github.com>
2020-02-28 13:26:48 +0800

    Update Application.cpp

--- DM2122_Week16_Files - Copy/DM2122 Prac/Source/Application.cpp
+++ DM2122_Week16_Files - Copy/DM2122 Prac/Source/Application.cpp
@@ -103,30 +103,27 @@ void Application::Init()
 void Application::Run()
 {
 	//Main Loop
+	SceneManager* sceneManager = SceneManager::getInstance();
 
-	Scene* scene = new SceneSkybox();
-	Scene* scenegame = new Minigame();
-
-	scene->Init();
-	scenegame->Init();
+	sceneManager->AddScene(new SceneSkybox());
+	sceneManager->AddScene(new SceneCarSelection());
+	sceneManager->AddScene(new Minigame());
+	sceneManager->AddScene(new SceneInterior());
 
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame
 	while (!glfwWindowShouldClose(m_window) && !IsKeyPressed(VK_ESCAPE))
 	{
-		scenegame->Update(m_timer.getElapsedTime());
-		scenegame->Render();
-		//SetCursorPos(750, 500);
+		sceneManager->Update(m_timer.getElapsedTime());
+
 		//Swap buffers
 		glfwSwapBuffers(m_window);
 		//Get and organize events, like keyboard and mouse input, window resizing, etc...
 		glfwPollEvents();
-        m_timer.waitUntil(frameTime);       // Frame rate limiter. Limits each frame to a specified time in ms.   
+		m_timer.waitUntil(frameTime);       // Frame rate limiter. Limits each frame to a specified time in ms.   
 
 	} //Check if the ESC key had been pressed or if the window had been closed
-	scene->Exit();
-	scenegame->Exit();
-	delete scene;
-	delete scenegame;
+
+	sceneManager->DeleteAllScenes();
 }
 void Application::Exit()
 {
