d9baa0e5b68f7c88edce12376bf0dde8c8d142ab
Ethan148 <54051459+Ethan148@users.noreply.github.com>
2020-02-28 16:33:51 +0800

    update to the new scene manager

--- DM2122_Week16_Files - Copy/DM2122 Prac/Source/Application.cpp
+++ DM2122_Week16_Files - Copy/DM2122 Prac/Source/Application.cpp
@@ -105,10 +105,7 @@ void Application::Run()
 	//Main Loop
 	SceneManager* sceneManager = SceneManager::getInstance();
 
-	sceneManager->AddScene(new SceneSkybox());
-	sceneManager->AddScene(new SceneCarSelection());
-	sceneManager->AddScene(new Minigame());
-	sceneManager->AddScene(new SceneInterior());
+	sceneManager->SetNextScene(SceneManager::SCENE_SKYBOX);
 
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame
 	while (!glfwWindowShouldClose(m_window) && !IsKeyPressed(VK_ESCAPE))
