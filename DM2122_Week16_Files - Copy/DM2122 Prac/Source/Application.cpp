5819df47f33c2f20f9f2f86b15cd06b4ad6b2158
Ethan148 <54051459+Ethan148@users.noreply.github.com>
2020-02-28 17:07:17 +0800

    minor change

--- DM2122_Week16_Files - Copy/DM2122 Prac/Source/Application.cpp
+++ DM2122_Week16_Files - Copy/DM2122 Prac/Source/Application.cpp
@@ -104,8 +104,6 @@ void Application::Run()
 	//Main Loop
 	SceneManager* sceneManager = SceneManager::getInstance();
 
-	sceneManager->SetNextScene(SceneManager::SCENE_SKYBOX);
-
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame
 	while (!glfwWindowShouldClose(m_window) && !IsKeyPressed(VK_ESCAPE))
 	{
